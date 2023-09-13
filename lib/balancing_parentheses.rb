d4c548e4fd600405360633cd508fe00da6c97c30
eugene <eugeneholenstein@gmail.com>
2023-09-13 14:07:39 +1000

    commit tree 76313865322a48ca385e3db2dc93a3e08377f4f9

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,31 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(string)
+    stack = Stack.new
+    parens = { "(" => ")", ")" => "("}
+
+    opening_parens = parens.keys
+    closing_parens = parens.values
+
+    string.chars.each do |char|
+        if opening_parens.include?(char)
+            stack << ch
+        elsif closing_parens.include?(char)
+            char == parens[stack.last] ? stack.pop : (return false)
+        end
+    end
+        stack.empty?
+    # stack.push(char) 
+    #     if char == "("
+    #         open += 1
+    #     elsif char == ")"
+    #         close += 1
+    #     end
+    # end
+
+    # open == close ? 0 : open > close ? open - close : close - open
+    # if stack.size
+    # stack.size
+end
